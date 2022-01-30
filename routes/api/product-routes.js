fef37530d140badef35066967c409b3aa497f2e3
Wenwen Tian <wwtian9@gmail.com>
2022-01-30 10:53:25 -0700

    get one products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -11,15 +11,25 @@ router.get("/", (req, res) => {
     include: [{ model: Category }, { model: Tag }],
   })
     .then((productData) => res.json(productData))
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+    .catch((err) => res.status(500).json(err));
 });
 
 // get one product
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: { id: req.params.id },
+    include: [{ model: Category }, { model: Tag }],
+  })
+    .then((productData) => {
+      if (!productData) {
+        res.status(404).json({ message: "No product found with this id" });
+        return;
+      }
+      res.json(productData);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 // create new product
